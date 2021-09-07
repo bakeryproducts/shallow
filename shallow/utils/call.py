bb91476ec17eb33adcd15fcc9a48ba315e7342ae
bakeryproducts <gleb.m.sokolov@gmail.com>
2021-09-07 17:36:25 +0300

    pep

--- shallow/utils/call.py
+++ shallow/utils/call.py
@@ -5,11 +5,13 @@ def on_master(f):
             return f(*args)
     return wrapper
 
+
 def on_epoch_step(f):
     def wrapper(*args):
         if (args[0].L.n_epoch % args[0].L.step) == 0:
             return f(*args)
     return wrapper
+
 
 def on_train(f):
     def wrapper(*args):
@@ -17,11 +19,13 @@ def on_train(f):
             return f(*args)
     return wrapper
 
+
 def on_validation(f):
     def wrapper(*args):
         if not args[0].L.model.training:
             return f(*args)
     return wrapper
+
 
 def get_cb_by_instance(cbs, cls):
     for cb in cbs:
